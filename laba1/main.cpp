9ec7b47b01afc7ca5aa5a5f1472bc8bb5e00da40
SofiyaAndreyeva <137882389+SofiyaAndreyeva@users.noreply.github.com>
2023-09-17 21:16:17 +0300

    Add files via upload

---
+++ laba1/main.cpp
@@ -0,0 +1,13 @@
+#include "colors.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Colors w;
+    w.setMaximumSize(488,328);
+    w.setMaximumSize(488,328);
+    w.show();
+    return a.exec();
+}
